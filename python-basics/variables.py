8771440057e39afa0d4f76cf631f35cfeb5190bd
Maddy <na.gaertner@web.de>
2026-07-12 09:20:58 +0200

    work on script

--- python-basics/variables.py
+++ python-basics/variables.py
@@ -18,6 +18,12 @@ print("Höhe als Ganzzahl:",hoehe_int)
 # Listen sind eine Art Conatiner, in dem mehrere Werte gespeichert werden können.
 meine_zahlen_liste = [1, 2, 3, 4, 5]
 meine_buchstaben_liste = ["a", "b", "c", "d", "e"]
+meine_gemischte_liste = [1, "a", 2, "b", 3, "c"]
 
 print("Zahlenliste:", meine_zahlen_liste)
 print("Buchstabenliste:", meine_buchstaben_liste)
+print("Gemischte Liste:", meine_gemischte_liste)
+
+# add new value to the list
+meine_zahlen_liste. append(6)
+print("Updated Zahlenliste:", meine_zahlen_liste)
