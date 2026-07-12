10712b1dbc6e227c17b0fa8fc9a1889aac39ed98
Maddy <na.gaertner@web.de>
2026-07-12 10:35:47 +0200

    complete variables

---
+++ python-basics/exercises/variables-exercises.py
@@ -0,0 +1,13 @@
+# Aufgabe: 
+# Lege eine Integer-Variable meine-variable mit dem Wert 5 an,
+# caste diese in eine Float-Variable und gebe sie aus
+
+# Integer-Variable
+my_variable = 5
+print("My variable:", my_variable)
+
+my_variable_str = str(my_variable)
+print("My Variable as String:", my_variable_str)
+
+my_variable_int = int(my_variable)
+print("My variable as Integer:", my_variable_int)
--- python-basics/variables.py
+++ python-basics/variables.py
@@ -5,11 +5,12 @@ name =  "Maddy" #String = Wörter/Buchstaben
 print(name) # Werte ausgeben
 alter = 40 #Integer = ganze Zahlen
 print(alter) # Werte ausgeben
-# Casting
-alter_str = str(alter) #Casting = Umwandlung von einem Datentyp in einen anderen
+# Casting  = Umwandlung von einem Datentyp in einen anderen
+alter_str = str(alter) #String = Wörter/Text
+print("Alter als String:", alter_str) # Werte ausgeben
 
 # Gleitkommazahlen zu Ganzzahlen casten
-hoehe = 175.5 #Float = Kommazahlen
+hoehe = 175.5 #Float = Kommazahlen zu Ganzenzahlen
 hoehe_int = int(hoehe) # Float zu Integer
 
 # Ergebnis ausgeben
@@ -26,4 +27,35 @@ print("Gemischte Liste:", meine_gemischte_liste)
 
 # add new value to the list
 meine_zahlen_liste. append(6)
+
 print("Updated Zahlenliste:", meine_zahlen_liste)
+print("1. Wert in der Liste meine_zahlen_liste:", meine_zahlen_liste[0]) #immer bei 0 anfangen zu zählen
+print("2. Wert in der Liste meine_zahlen_liste:", meine_zahlen_liste[1])
+print("3. Wert in der Liste meine_zahlen_liste:", meine_zahlen_liste[2])    
+
+# change value
+meine_zahlen_liste[0] = 0
+print("Updated Zahlenliste:", meine_zahlen_liste)
+
+# remove value
+meine_zahlen_liste.remove(0)
+print("Updated Zahlenliste nach dem Löschen:", meine_zahlen_liste)
+
+#lenght of list
+print("Länge der Zahlenliste:", len(meine_zahlen_liste))
+
+mein_dictonary = {"name": "Maddy", "alter": 25}
+print("Name:", mein_dictonary["name"])
+
+# value change
+mein_dictonary["name"] = "Nathalie"
+print("Neuer Name:", mein_dictonary["name"])
+
+# add new key-value
+mein_dictonary["hobby"] = "schwimmen"
+print("Hobby:", mein_dictonary["hobby"])
+
+# delete key-value
+print("mein_dictonary before delete:", mein_dictonary)
+del mein_dictonary["hobby"]
+print("mein_dictonary after delete:", mein_dictonary)
