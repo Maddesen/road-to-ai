7ea66c6056ed0e24e7265c79eba991d61d6d4403
Maddy <na.gaertner@web.de>
2026-07-20 09:06:42 +0200

    argparse

---
+++ python-basics/Argparse.py
@@ -0,0 +1,15 @@
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Beispiel für argparse")
+parser.add_argument("pflicht_int", help="Integer der erforderlich ist", type=int)
+parser.add_argument("--optional_int", help="Optionaler Integer", type=int)
+parser.add_argument("--optional_str", help="Optional String")
+parser.add_argument("--wahl", help="Meine Wahl", choices=["a", "b", "c", "d"])
+args = parser.parse_args()
+
+print("Integer:", args.pflicht_int, "- Typ", type(args.pflicht_int))
+print("Optional Integer:", args.optional_int)
+print("Optional String:", args.optional_str, "- Type:", type(args.optional_str))
+print("Wahl:", args.wahl)
+print("Ende")
---
+++ python-basics/exercises/argparse_calculator.py
@@ -0,0 +1,11 @@
+# Aufgabe: Skript erstellem mit einfachen mathematischen Operationen mit Argparse
+
+# 1. zahl1 + zahl2 sollen vom Skript als Pflichtargument akzetpiert werden. Ganzzahlen
+
+# 2. Es soll ein optionales Argumen '--operation' geben, für +, -, *, /
+# wenn nichts angegeben wird, soll immer addiert werden
+
+# 3. Implementieren Sie die Berechnungslogik
+
+# 4. Ergebnis ausgaben
+
